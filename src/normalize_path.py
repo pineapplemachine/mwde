31be499eee6d2d0826f5133c99f64ab8f79ebc39
Sophie Kirschner <sophie@thehumangeo.com>
2018-08-21 09:35:38 +0300

    Improve path normalization to cover more cases

--- src/normalize_path.py
+++ src/normalize_path.py
@@ -1,10 +1,12 @@
 def normalize_path(path):
     if not path:
         return path
+    path = path.replace("\\ ", " ")
+    path = path.replace("\\'", "'")
+    path = path.replace("\\\"", "\"")
     if path[0] == '"' and path[-1] == '"':
         path = path[1:-1]
     elif path[0] == "'" and path[-1] == "'":
         path = path[1:-1]
-    path = path.replace("\\ ", " ")
     return path
     
