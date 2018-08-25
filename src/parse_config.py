68cc9247878ab5bfff1ef242e99def462ccc6b2c
Sophie Kirschner <pineapplemachine@gmail.com>
2018-08-25 14:42:28 +0300

    Fix config parsing, for real this time

--- src/parse_config.py
+++ src/parse_config.py
@@ -14,16 +14,16 @@ def parse_config(config_path):
     config_parser.read(config_path)
     config_object = {}
     if config_parser.has_option("colors", "substring_colors"):
-        value = config_parser.get("colors", "substring_colors")[-1].lower()
+        value = config_parser.get("colors", "substring_colors")
         config_object["substring_highlighting"] = get_boolean(value)
     if config_parser.has_option("colors", "script_colors"):
-        value = config_parser.get("colors", "script_colors")[-1].lower()
+        value = config_parser.get("colors", "script_colors")
         config_object["script_syntax_highlighting"] = get_boolean(value)
     if config_parser.has_option("colors", "function_colors"):
-        value = config_parser.get("colors", "function_colors")[-1].lower()
+        value = config_parser.get("colors", "function_colors")
         config_object["info_function_highlighting"] = get_boolean(value)
     if config_parser.has_option("colors", "npcat_colors"):
-        value = config_parser.get("colors", "npcat_colors")[-1].lower()
+        value = config_parser.get("colors", "npcat_colors")
         config_object["npcat_list_highlighting"] = get_boolean(value)
     if config_parser.has_option("load_file_paths", "paths"):
         config_object["load_paths"] = list(filter(None, (
